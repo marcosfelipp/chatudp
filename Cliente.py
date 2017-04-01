693075da5e2063d226f9e6523a0ddbcb70111382
marcosfelipp <mfelipe11@yahoo.com.br>
2017-03-31 21:16:34 -0300

    v1

---
+++ Cliente.py
@@ -0,0 +1,34 @@
+from socket import *
+import pickle
+
+minhaListaDeContatos = []
+
+serverHost = 'localhost'
+serverPort = 6439
+sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj.connect((serverHost, serverPort))
+
+print('----------AUTENTICAÇÂO------------')
+print('Digite seu endereco de email')
+email = input()
+print('Digite sua senha')
+senha = input()
+
+#Autenticação com servidor:
+mensagem = [1, email, senha]
+serialize = pickle.dumps(mensagem)
+sockobj.send(serialize)
+
+while True:
+    print('-------------MENSAGEM---------------')
+    print("Digite 1 para conectar com um cliente da sua lista de contatos\nDigite 2 para solicitar um contato")
+    selecao = int(input())
+    if selecao == 1:
+        print('Sua lista de contatos:')
+        print(minhaListaDeContatos)
+        print('Digite o numero contato que queira conectar')
+        n = int(input())
+    else :
+        if selecao == 2:
+            print('Digite um e mail para buscar no servidor')
+            n = int(input())
---
+++ Servidor.py
@@ -0,0 +1,46 @@
+from socket import *
+import threading
+import pickle
+
+meuHost = ''
+minhaPorta = 6439
+
+listaCOnexoes = []
+
+class conexaoTCP(threading.Thread):
+
+    #id: número de identificacao do cliente
+    def __init__(self,id,com,IPCliente):
+        threading.Thread.__init__(self)
+        self.id = id
+        self.com = com
+        self.IPCliente = IPCliente
+        print ('conectado com o cliente', IPCliente)
+    def run(self):
+        print('starting thread')
+        serializado = self.com.recv(1024)
+        mensagem = pickle.loads(serializado)
+        tipoMensagem = mensagem[0]
+
+        #tratamento de mensagens recebidos:
+        if tipoMensagem == 1:
+            tupla = [mensagem[1],mensagem[2]]
+            listaCOnexoes.append(tupla)
+        else :
+            if tipoMensagem == 2:
+                print('2')
+
+
+
+#Criando sockets:
+sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj.bind((meuHost, minhaPorta))
+sockobj.listen(5)
+
+while True:
+    print(listaCOnexoes)
+    conexao, IPCliente = sockobj.accept()
+    thread1 = conexaoTCP(1, conexao, IPCliente)
+    thread1.start()
+
+sockobj.close
